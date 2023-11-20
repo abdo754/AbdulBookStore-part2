b14116d96fc942eaa360cdfbab14e2950a2a34d0
W0786364 <W0786364@ADL-A0211Z-37>
2023-11-20 12:44:30 -0500

    i have made connection to category and icatogroy

---
+++ AbdulBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using AbdulBooks.DataAccess.Repository.IRepository;
+using AbdulBooks.Models;
+using AbdulBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbdulBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ AbdulBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using AbdulBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbdulBooks.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository :IRepository<Category>
+    {
+        void update(Category category);
+    }
+}
